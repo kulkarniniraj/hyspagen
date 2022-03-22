509614bca3e487be447264b97ccacfde5601f852
Niraj Kulkarni <kulkarniniraj14@gmail.com>
2022-03-22 20:57:22 +0530

    Added alerts for create and update operation

--- static/updateinfo.js
+++ static/updateinfo.js
@@ -11,8 +11,21 @@ function getFields(){
     return out
 }
 
+function Alert(props){
+    return html`
+        <div class="alert ${props.type == 1? 
+            'alert-success': 'alert-danger'} alert-dismissible fade show" role="alert">
+            ${props.message}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close" onClick=${() => props.onDismiss({state: 0, msg: ''})}></button>
+        </div>
+
+    `
+}
+
 function Create(props){
     const [state, setState] = useState({});
+    // alert: 0 -> disable, 1 -> success, 2 -> fail
+    const [alert, setAlert] = useState({state: 0, msg: 'test 3'}); 
     let fields = getFields();
     Object.keys(fields).forEach(key => {
         state[key] = ''
@@ -25,9 +38,11 @@ function Create(props){
         axios.post('create', state).then(res => {
             console.log(res)
             setState({})
+            setAlert({state: 1, msg: `Successfully created ${NAME}`})
 
         }).catch(err => {
             console.log(err.response)
+            setAlert({state: 2, msg: `Error: ${err.response.data.desc}`})
         })
     }
     const onChange = key => {
@@ -41,6 +56,10 @@ function Create(props){
         <div class="row mt-4">
             <div class="col-3"></div>
             <div class='col-5 '>
+                ${alert.state == 0? '' : 
+                    html`<${Alert} type=${alert.state} message=${alert.msg}
+                        onDismiss=${setAlert}/>`
+                }
                 <h2>Add ${NAME}</h2>
                 <form class="border border-secondary p-4">
                     <div class="mb-3">
@@ -60,12 +79,17 @@ function Create(props){
 
 function Update(props){
     let [data, setData] = useState({})
+    // alert: 0 -> disable, 1 -> success, 2 -> fail
+    const [alert, setAlert] = useState({state: 0, msg: 'test 3'}); 
     let getUserData = () => {
         axios.get(`get/${props.id}`)
             .then(resp => {
                 console.log('update data:', resp.data.data)
                 setData(resp.data.data)
+            }).catch(err => {
+                console.log(err.response)
             })
+ 
 
     }
 
@@ -73,9 +97,10 @@ function Update(props){
         console.log('submitting', data)
         ev.preventDefault()
         axios.post('update', data).then(res => {
-
+            setAlert({state: 1, msg: `Successfully updated ${NAME}`})
         }).catch(err => {
             console.log(err.response)
+            setAlert({state: 2, msg: `Error: ${err.response.data.desc}`})
         })
     }
     const onChange = key => {
@@ -92,6 +117,10 @@ function Update(props){
         <div class="row mt-4">
             <div class="col-3"></div>
             <div class='col-5 '>
+                ${alert.state == 0? '' : 
+                    html`<${Alert} type=${alert.state} message=${alert.msg}
+                        onDismiss=${setAlert}/>`
+                }
                 <h2>Update ${NAME}</h2>
                 <form class="border border-secondary p-4">
                     <div class="mb-3">
